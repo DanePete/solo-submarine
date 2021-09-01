72a2cff95331ad680be25d3aff9c55eb6470e28c
dane <dane.m.petersen@gmail.com>
2021-09-01 15:19:47 -0500

    rewrote queries

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -6,13 +6,32 @@ router.get('/', (req, res) => {
   // what is the value of req.user????
   console.log('req.user:', req.user);
 
-  pool
-    .query(`SELECT * FROM "secret";`)
+  if(req.user.clearance_level <= 10) {
+    let queryText=
+    `
+    SELECT * FROM secret
+    WHERE secrecy_level <= 10
+    `
+    pool
+    .query(queryText)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
       res.sendStatus(500);
     });
+  } else {
+    let queryText=
+    `
+    SELECT * FROM "secret"
+    `
+    pool
+    .query(queryText)
+    .then((results) => res.send(results.rows))
+    .catch((error) => {
+      console.log('Error making SELECT for secrets:', error);
+      res.sendStatus(500);
+    });
+  }
 });
 
 module.exports = router;
